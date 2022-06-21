a238e61ab52c6bbaf9201a1498a6100f2a6822d8
Prathammehra7 <manshumehra7@gmail.com>
2022-06-21 13:47:51 +0530

    update

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+// const arr=["manshu,karan,shiva,krishna"]
+// console.log(arr[3]);
+const arr=[1,2,3,4,5,6,76,5];
+
+// first method
+// for (let i = 0; i< arr.length; i++)
+// {
+//     console.log(arr[i]);
+
+// }
+
+// second method
+// arr.forEach((elementss) => {
+//     console.log(elementss);
+//   });
+
+// third method
+// arr.map((element) => {
+//     console.log(element);
+//   });
+
+// forth method
+console.log(new Date().getTime());
+
